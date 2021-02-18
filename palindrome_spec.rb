a3e57f761936de5db8ab9e386e71b07ba50137f2
Connieh1 <connie2630a@gmail.com>
2021-02-18 11:53:04 -0800

    Write test for truthy context

--- palindrome_spec.rb
+++ palindrome_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe Palindrome do
 
   context "test word is a palindrome" do
     it "should read the same forwards as backwards" do 
+      test_word = "Madam"
+      result = Palindrome.palindrome?(test_word)
+      expect(result).to be_truthy
     end
   end 
 
