1fe43fadb308e6de765b5792e53d69ebbe20fdb7
Connieh1 <connie2630a@gmail.com>
2021-02-18 11:47:47 -0800

    Begin writing tests in palindrome_spec.rb

---
+++ palindrome_spec.rb
@@ -0,0 +1,15 @@
+require_relative "palindrome"
+
+RSpec.describe Palindrome do
+
+  context "test word is a palindrome" do
+    it "should read the same forwards as backwards" do 
+    end
+  end 
+
+  context "test word is NOT a palindrome" do
+    it "should NOT read the same forwards as backwards" do 
+    end
+  end
+
+end
