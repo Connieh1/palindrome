dc9ff2d2218a20ec6df484316c2e11870d09316d
Connieh1 <connie2630a@gmail.com>
2021-02-18 12:39:30 -0800

    Add logic for checking for palindrome

--- palindrome.rb
+++ palindrome.rb
@@ -1,4 +1,17 @@
 class Palindrome
 
+  def self.palindrome?(test_word)
+    test_word = test_word.downcase
+    word_length = test_word.length
+    mid = word_length / 2
+    front = test_word.slice(0,mid)
+    back = (test_word.split('').reverse().join('')).slice(0,mid)
+    if front === back
+      true
+    else
+      false
+    end
+
+  end
 
 end
--- palindrome_spec.rb
+++ palindrome_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Palindrome do
   context "test word is a palindrome" do
     it "should read the same forwards as backwards" do 
       test_word = "Madam"
-      result = Palindrome.palindrome?(test_word)
+      result = Palindrome.palindrome? test_word
       expect(result).to be_truthy
     end
   end 
@@ -13,7 +13,7 @@ RSpec.describe Palindrome do
   context "test word is NOT a palindrome" do
     it "should NOT read the same forwards as backwards" do 
       test_word = "Food"
-      result = Palindrome.palindrome?(test_word)
+      result = Palindrome.palindrome? test_word
       expect(result).to be_falsey
     end
   end
